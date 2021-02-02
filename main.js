2d9651b103fbedefe2d3552e478f284554626e4c
Henry Gruett <henrikgruett@gmail.com>
2021-02-02 15:36:07 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,23 +1,30 @@
 
 window.onload = function onLoad() {
-  var line = new ProgressBar.Line('#progress', {
+  var line = new ProgressBar.Line('#yProgress', {
     color: '#FFEA82',
     from: {color: '#FFEA82'},
     to: {color: '#ED6A5A'},
   });
+  /*
+ window.onload = function onLoad() {
+  var line = new ProgressBar.Line('#mProgress', {
+    color: '#FFEA82',
+    from: {color: '#FFEA82'},
+    to: {color: '#ED6A5A'},
+  });*/
 
   function progress() {
     var now = new Date();
-    var start = new Date(now.getFullYear(), 0, 1);  // Start of this year
-    var end = new Date(now.getFullYear() + 1, 0, 1);  // End of this year
-    var done = (now-start) / (end-start);
-    var percentStr = (100.0 * done).toString();
-    if (done < 0.1) {
-      percentStr = percentStr.slice(0, 9);
+    var yStart = new Date(now.getFullYear(), 0, 1);  // Start of this year
+    var yEnd = new Date(now.getFullYear() + 1, 0, 1);  // End of this year
+    var yDone = (now-yStart) / (yEnd-yStart);
+    var yPercentStr = (100.0 * done).toString();
+    if (yDone < 0.1) {
+      yPercentStr = yPercentStr.slice(0, 9);
     } else {
-      percentStr = percentStr.slice(0, 10);
+      yPercentStr = yPercentStr.slice(0, 10);
     }
-    document.getElementById("percent").innerHTML = percentStr + "%";
+    document.getElementById("percent").innerHTML = yPercentStr + "%";
     return done;
   }
 
